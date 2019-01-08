c84574e94db1630361e97c4aff7b90430d35be9a
wuscier <1352288123@qq.com>
2019-01-08 22:30:35 +0800

    customize eletron window(frameless and no vertical scrollbar)

--- app/main.js
+++ app/main.js
@@ -8,13 +8,20 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600,icon:path.join(__dirname,'app.ico')})
+  mainWindow = new BrowserWindow({
+    width: 800, 
+    height: 600,
+    icon:path.join(__dirname,'app.ico'),
+    frame:false,
+    resizable:false,
+    
+  })
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
 
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+   mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
